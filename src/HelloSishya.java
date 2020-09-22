fca46dcfa13293041eeb056f6492dcf2e3c85ff6
ramas <ramas@192.168.0.13>
2020-09-22 11:03:50 -0700

    New Java Program

---
+++ src/HelloSishya.java
@@ -0,0 +1,11 @@
+
+public class HelloSishya {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Let us start exploring the world of technologies !!");
+
+	}
+
+}
